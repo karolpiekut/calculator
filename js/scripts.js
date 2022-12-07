d44354fcfc88345950d7e21096c25b02cf23a618
kpiekut <karol.piekut@pm.me>
2022-12-07 11:11:33 +0000

    add calculator basic functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,49 @@
-let buttons = document.querySelectorAll("button");
+/*let buttons = document.querySelectorAll("button");
 let screen = document.querySelector("#screen");
 Array.from(buttons).forEach(button =>
     button.addEventListener("click", usrSelect));
 
 function usrSelect(event) {
+    let arrVal = [];
     const select = event.target;
-    screen.innerText = select.value;
+    arrVal.push(select.value);
+    screen.innerText = arrVal;
+}
+*/
+
+function multiply(...par) {
+    return par.reduce(
+        (acc, curr) => acc * curr
+    );
 }
 
+function divide(...par) {
+    return par.reduce(
+        (acc, curr) => acc / curr
+    );
+}
 
+function add(...par) {
+    return par.reduce(
+        (acc, curr) => acc + curr
+    );
+}
 
-/*
-press a digit button
+function deduct(...par) {
+    return par.reduce(
+        (acc, curr) => acc - curr
+    );
+}
 
- */
+function deltaPercentage(par1, par2) {
+    return (par1 - par2) / par1 * 100;
+}
+
+function squareRoot(par) {
+    return Math.sqrt(par);
+}
+
+function percentage(par1) {
+    return par1 / 100;
+}
+

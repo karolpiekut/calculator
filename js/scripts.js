9f466b424d2d14b26789f002eb7334152af1b84a
kpiekut <karol.piekut@pm.me>
2022-12-09 15:32:07 +0000

    revert back class naming

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,53 @@
-let digits = document.querySelectorAll(".number");
-let screen = document.querySelector("#screen");
-let operations = document.querySelectorAll(".operations")
-
-Array.from(digits).forEach(button =>
-    button.addEventListener("click", usrSelect));
-
-Array.from(operations).forEach(button =>
-    button.addEventListener("click", usrSelect));
-
-function usrSelect(event) {
-    let arrVal = [];
-    const select = event.target;
-    arrVal.push(select.value);
-    screen.innerText = arrVal;
-}
-
-
+/* calculator functions */
 function multiply(...par) {
     return par.reduce(
         (acc, curr) => acc * curr
     );
 }
-
 function divide(...par) {
     return par.reduce(
         (acc, curr) => acc / curr
     );
 }
-
 function add(...par) {
     return par.reduce(
         (acc, curr) => acc + curr
     );
 }
-
 function deduct(...par) {
     return par.reduce(
         (acc, curr) => acc - curr
     );
 }
-
 function deltaPercentage(par1, par2) {
     return (par1 - par2) / par1 * 100;
 }
-
 function squareRoot(par) {
     return Math.sqrt(par);
 }
-
 function percentage(par1) {
     return par1 / 100;
 }
-
 function operate(fun, par1, par2) {
     return fun(par1, par2);
 }
+/* event listeners and query selectors */
+let buttons = document.querySelectorAll("button");
+let screen = document.querySelector("#screen");
+
+
+Array.from(buttons).forEach(button =>
+    button.addEventListener("click", theActualWork));
+
+/* the meat */
+
+function theActualWork(button) {
+    let arrVal = [];
+    const select = button.target;
+    arrVal.push(select.value);
+    screen.innerText = arrVal;
+    console.log(arrVal);
+
+}
+
 
